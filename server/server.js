2b7be04dc11ffd1f41276dbb4a042e495bc75f80
Sushant007-github <85660006+Sushant007-github@users.noreply.github.com>
2026-02-28 12:29:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ app.use(cors({
   origin: '*',
   credentials: false
 }));
-}));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
